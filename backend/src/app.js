1205c63032a312a2cc60ddc8e759060b40343553
JerryLmr <jerrylmr@icloud.com>
2026-01-17 01:26:08 +0100

    connect to db

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,17 @@
 import express from "express";
-
+import pool from "./models/db.js";
 const app = express();
 
 app.use(express.json());
 
+(async () => {
+  try {
+    const [rows] = await pool.query("select 1");
+    console.log("✅ MySQL connected");
+  } catch (err) {
+    console.error("❌ MySQL connection failed:", err.message);
+  }
+})();
 // health check
 app.get("/api/health", (req, res) => {
   res.json({ status: "ok" });
